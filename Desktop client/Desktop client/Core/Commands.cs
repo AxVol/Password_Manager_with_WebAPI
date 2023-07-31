d5518023855d8fdd007b30bd1db9ca1d7a589da3
AxVol <ax_vol@mail.ru>
2023-07-31 22:26:14 +0300

    init desctop client proj and add MVVM

---
+++ Desktop client/Desktop client/Core/Commands.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TFT_TEAM_BUILDER.Core
+{
+    public class Commands : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public Commands(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            execute = Execute;
+            canExecute = CanExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute == null || CanExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
---
+++ Desktop client/Desktop client/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using TFT_TEAM_BUILDER.Core;
+
+namespace Desktop_client.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private MainViewModel mainViewModel { get; set; }
+
+        private object currentView;
+
+        public object CurrentView
+        {
+            get => currentView;
+            set 
+            { 
+                currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() 
+        {
+            mainViewModel = this;
+        }
+    }
+}
